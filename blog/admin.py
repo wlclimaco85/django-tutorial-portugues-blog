2ba44b847c93b6d5b89d7809a1dc036936b8dc06
washington.costa@sankhya.com.br <washington.costa@sankhya.com.br>
2021-11-01 11:39:42 -0300

    teste

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "author", "created", "updated")
     prepopulated_fields = {"slug": ("title",)}
+
+@admin.register(Parceiro)
+class ParceiroAdmin(admin.ModelAdmin):
+    list_display = ("nome", "cnpj", "author","status", "created", "updated")
+    prepopulated_fields = {"cnpj": ("nome",)}
+
+@admin.register(Xmls)
+class XmlsAdmin(admin.ModelAdmin):
+    list_display = ("nomeFile", "cnpj", "xml", "author","status", "created", "updated")
+    prepopulated_fields = {"cnpj": ("nomeFile",)}
+
+@admin.register(StatusMaquinas)
+class StatusMaquinasAdmin(admin.ModelAdmin):
+    list_display = ("nomeMaquina", "status", "author", "dtUltVezOnLine")
+    prepopulated_fields = {"nomeMaquina": ("dtUltVezOnLine",)}
