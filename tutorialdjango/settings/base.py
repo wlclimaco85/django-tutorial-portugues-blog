902a4a8cdb0bd5757a54edc00f267a3f9d43dc0f
washington.costa@sankhya.com.br <washington.costa@sankhya.com.br>
2021-11-06 21:59:36 -0300

    ddd

--- tutorialdjango/settings/base.py
+++ tutorialdjango/settings/base.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*','192.168.100.190','192.168.3.1','192.168.181.1']
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+   # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
